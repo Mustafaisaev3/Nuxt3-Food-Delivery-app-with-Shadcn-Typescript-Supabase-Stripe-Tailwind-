1abd60b0292145a3db9be0947b339a1fa2b2503f
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-11-28 22:44:40 +0300

    Making basic setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  modules: ['@nuxtjs/tailwindcss'],
+  // @ts-ignore
+  components: [
+    {
+      path: '~/components/ui',
+      // this is required else Nuxt will autoImport `.ts` file
+      extensions: ['.vue'],
+      // prefix for your components, eg: UiButton
+      prefix: ''
+    },
+  ],
+  devtools: { enabled: true },
 })
