c34b01ce710de84d443d4abbfa324dfff7910e9a
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-11-29 19:04:15 +0300

    Making Sidebar active style

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,24 +1,47 @@
 <template>
   <div class="space-y-4 py-4 flex-col flex items-center h-full bg-[#111016] shadow-sm border-r">
-    <div class="h-full px-3 py-2 flex flex-col justify-between">
+    <div class="w-full h-full py-2 flex flex-col justify-between">
         <div class="flex flex-col flex-1 gap-[30px] ">
-            <div class="pb-[20px]">
-                <Icon name="mdi:align-vertical-distribute" size='25' color='white'/>
+            <div class="w-full flex justify-center items-center pb-[20px]">
+                <Icon name="mdi:food" size='30' color='white' class="cursor-pointer"/>
             </div>
-            <NuxtLink to='/'>
+            <NuxtLink 
+              to='/' 
+              class="w-full flex justify-center items-center py-2" 
+              :class="{'border-l-4 border-[#c7def0]': isRouteActive('/')}"
+            >
                 <IconCSS name="mdi:home" size='25' color='white' class="text-[white] hover:text-[#e6e6e6]"/>
             </NuxtLink>
-            <NuxtLink to='/pizza'>
+            <NuxtLink 
+              to='/pizza' 
+              class="active w-full flex justify-center items-center py-2"
+              :class="{'border-l-4 border-[#c7def0]': isRouteActive('/pizza')}"
+            >
                 <IconCSS name="mdi:pizza" size='25' color='white' class="text-[white] hover:text-[#e6e6e6]"/>
             </NuxtLink>
-            <NuxtLink to='/user'>
+            <NuxtLink 
+              to='/user' 
+              class="w-full flex justify-center items-center py-2"
+              :class="{'border-l-4 border-[#c7def0]': isRouteActive('/user')}"
+            >
                 <IconCSS name="mdi:account" size='25' color='white' class="text-[white] hover:text-[#e6e6e6]"/>
             </NuxtLink>
-            <NuxtLink to='/orders'>
+            <NuxtLink 
+              to='/orders' 
+              class="w-full flex justify-center items-center py-2"
+              :class="{'border-l-4 border-[#c7def0]': isRouteActive('/orders')}"
+            >
                 <IconCSS name="mdi:clipboard-clock" size='25' class="text-[white] hover:text-[#e6e6e6]"/>
             </NuxtLink>
+            <NuxtLink 
+              to='/setting' 
+              class="w-full flex justify-center items-center py-2"
+              :class="{'border-l-4 border-[#c7def0]': isRouteActive('/setting')}"
+            >
+                <IconCSS name="mdi:cog" size='25' class="text-[white] hover:text-[#e6e6e6]"/>
+            </NuxtLink>
         </div>
-        <div>
+        <div class="w-full flex justify-center py-2 cursor-pointer">
             <Icon name="mdi:logout" size='25' color='white'/>
         </div>
     </div>
@@ -26,9 +49,16 @@
 </template>
 
 <script setup>
+const route = useRoute()
+
+console.log(route.path)
+
+const isRouteActive = (path) => {
+    return route.path == path ? true : false
+}
 
 </script>
 
-<style>
+<style scoped>
 
 </style>
---
+++ pages/pizza.vue
@@ -0,0 +1,14 @@
+<template>
+  
+  Pizzas
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
